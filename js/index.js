79371674991213dfb0d8c78fdd581095e6d59995
mk7700 <66050813+mk7700@users.noreply.github.com>
2020-09-17 14:14:58 +0900

    data and method

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'vue_tuto'
+        person: {
+            name: 'vue_tuto',
+            age: 27
+        }        
+    },
+    methods: {
+        nextYear(greeting) {
+            return greeting + '! ' + this.person.name + '는 내년에' + (this.person.age + 1) + '살 이됩니다.';
+        },
+        otherMethod: function() {
+            this.nextYear();
+        }
     }
 });
