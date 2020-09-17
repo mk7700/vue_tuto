6e158aad6fc4fd2046429781589a29bfcb517192
mk7700 <66050813+mk7700@users.noreply.github.com>
2020-09-17 14:32:06 +0900

    Data Binding

--- js/index.js
+++ js/index.js
@@ -4,9 +4,15 @@ new Vue({
         person: {
             name: 'vue_tuto',
             age: 27
-        }        
+        },
+        inputData: 'hello',
+        type: 'text',
+        Link: 'https://www.youtube.com/'        
     },
     methods: {
+        getYouTubeLink(channel) {
+            return this.Link + channel;
+        },
         nextYear(greeting) {
             return greeting + '! ' + this.person.name + '는 내년에' + (this.person.age + 1) + '살 이됩니다.';
         },
